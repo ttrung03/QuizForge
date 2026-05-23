4f3ebb1da1863946e4ca2a9b2482bfccd3ad10d5
TTrung <vothanhtrung9379@gmail.com>
2026-05-23 21:54:05 +0700

    feat: skip duplicate questions during import

--- Application/Interfaces/ICauHoiRepository.cs
+++ Application/Interfaces/ICauHoiRepository.cs
@@ -6,6 +6,8 @@ public interface ICauHoiRepository
 {
     Task<List<CauHoi>> GetByPhanAsync(Guid maPhan);
     Task<int> GetMaxMaSoCauHoiAsync(Guid maPhan);
+    Task<HashSet<string>> GetExistingNoiDungAsync(Guid maPhan);
     Task AddWithAnswersAsync(CauHoi cauHoi, List<CauTraLoi> answers);
+    Task BulkImportAsync(List<CauHoi> cauHois, List<CauTraLoi> cauTraLois);
     Task SoftDeleteAsync(Guid id);
 }
